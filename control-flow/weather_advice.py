89feb36d9328ae29c1d12ee0dd2367dd9f33e195
chornate <155074527+chornate@users.noreply.github.com>
2024-09-18 13:20:29 +0300

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,5 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-# Provide clothing recommendations based on the weather
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
